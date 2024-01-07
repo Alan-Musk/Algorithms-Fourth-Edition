36e62ff3d1930c0cdf29cbaf12f67696e35c8b83
AlanMusk <alanmusk@163.com>
2024-01-07 17:58:00 +0800

    3.3

---
+++ Chapter_03/Ch_3_3_Balanced_Search_Tree/Practice_3_3_21.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class Practice_3_3_21 {
+    @Test
+    public void testRB(){
+        BST<Integer,String> test=new BST<>();
+//        test.put();
+    }
+}
