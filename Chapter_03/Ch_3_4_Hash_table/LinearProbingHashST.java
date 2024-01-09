e9007a495aec983c53396855ead9a1a9fa33c9fc
AlanMusk <alanmusk@163.com>
2024-01-09 22:04:37 +0800

    3.3

--- Chapter_03/Ch_3_4_Hash_table/LinearProbingHashST.java
+++ Chapter_03/Ch_3_4_Hash_table/LinearProbingHashST.java
@@ -5,5 +5,33 @@ public class LinearProbingHashST <Key,Value>{
     private Value[] values; //值
     public LinearProbingHashST(){
         keys=(Key[]) new Object[M];
+        values=(Value[]) new Object[M];
+    }
+    private int hash(Key key){
+        return (key.hashCode()&0x7ffffff) %M;
+    }
+    private void resize(){}
+
+    public void put(Key key,Value value)
+    {
+        if(N>=M/2) resize(2*M);
+        int i;
+        for(i=hash(key);keys[i]!=null;i=(i+1)%M){
+            if(keys[i].equals(key)){
+                values[i]=value;
+                return;
+            }
+        }
+        keys[i]=key;
+        values[i]=value;
+        N++;
+    }
+    public Value get(Key key){
+        for (int i = hash(key); keys[i]!=null ;i=(i+1)%M) {
+            if(keys[i].equals(key)){
+                return values[i];
+            }
+            return null;
+        }
     }
 }
